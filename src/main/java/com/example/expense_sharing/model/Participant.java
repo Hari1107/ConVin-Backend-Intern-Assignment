f49292b6ac82e777425a0c348e60fce69884c1ef
HariHaran K <hariharan.k1107@gmail.com>
2024-07-29 18:14:57 +0530

    Add

--- src/main/java/com/example/expense_sharing/model/Participant.java
+++ src/main/java/com/example/expense_sharing/model/Participant.java
@@ -15,4 +15,36 @@ public class Participant {
     private double percentage;
 
     // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
 }
--- src/main/java/com/example/expense_sharing/model/User.java
+++ src/main/java/com/example/expense_sharing/model/User.java
@@ -14,5 +14,37 @@ public class User {
     private String name;
     private String mobileNumber;
 
-    // getters and setters
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 }
